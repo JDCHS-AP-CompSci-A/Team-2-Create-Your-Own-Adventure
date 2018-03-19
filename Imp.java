de6e0788e2e4b8eb1ab4f6a07fef9ed8279beb89
SevereHayDHD <haydenhickman@sccstudents.org>
2018-03-19 13:03:38 -0600

    added damage and hit chance
    I made 2 new things for monster, they now have unique damages and hit chances.

--- Imp.java
+++ Imp.java
@@ -5,5 +5,7 @@ public class Imp extends Monster {
     Imp(){
     this.health = 111;
     this.name = "Imp";
+    this.damage = 17;
+    this.hit_chance = 0.7;
     }
 }
--- Monster.java
+++ Monster.java
@@ -1,7 +1,22 @@
 
 public class Monster {
-  int health;
+  double health;
   String name;
+  double damage;
+  double hit_chance;
+  
+  public double attack() {
+       double roll = Math.random();
+    
+       if (roll >= this.hit_chance) {
+           return this.damage;
+       }
+       else {
+           return 0.00;
+       }
 
+  }
+    
+  
 }
 
