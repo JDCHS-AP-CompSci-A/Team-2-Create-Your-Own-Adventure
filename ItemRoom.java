58ef2bb191138fff4fbabae21beb2f062ebcf31e
kyllief <kylliefurukawa@sccstudents.org>
2018-03-23 12:56:42 -0600

    Rename 'item' to 'weapon'

--- ItemRoom.java
+++ ItemRoom.java
@@ -3,6 +3,6 @@ public class ItemRoom extends Desc {
     public void fill_room(){
     Crypt crypt3 = new Crypt();
     crypt3.fill_Wcat();
-    this.item = crypt3.Weapon_db.get(0);    
+    this.weapon = crypt3.Weapon_db.get(0);    
     }
 }
